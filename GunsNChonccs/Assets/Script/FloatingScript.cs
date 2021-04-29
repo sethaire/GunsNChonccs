10948fb22d5c4dfe5f675ffd76d4efb8b100ca54
sethaire <sethairs@gmail.com>
2021-04-29 10:56:15 +0200

    idk

---
+++ GunsNChonccs/Assets/Script/FloatingScript.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FloatingScript : MonoBehaviour
+{
+    public float amplitude = 0.5f;
+    public float frequency = 1f;
+
+    Vector3 posOffset = new Vector3();
+    Vector3 tempPos = new Vector3();
+
+    void Start()
+    {
+        posOffset = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Float up/down with a Sin()
+        tempPos = posOffset;
+        tempPos.y += Mathf.Sin(Time.fixedTime * Mathf.PI * frequency) * amplitude;
+
+        transform.position = tempPos;
+    }
+}
